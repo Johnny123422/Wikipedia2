2f006111f8c99871ad44d8d761ac3b72e2d953e1
Johnny123422 <lupesantoneta@gmail.com>
2025-06-03 17:17:07 +0300

    Commit nou Ionut

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,20 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+document.getElementById("cautare-articol").addEventListener("input", function () {
+    let termen = this.value.toLowerCase().trim().normalize("NFD").replace(/[\u0300-\u036f]/g, '');
+
+    let articole = document.querySelectorAll(".articol-container");
+
+    articole.forEach(function (produs) {
+        let valNume = produs.querySelector(".val-nume").innerHTML
+            .toLowerCase().trim().normalize("NFD").replace(/[\u0300-\u036f]/g, '');
+
+        if (valNume.includes(termen)) {
+            produs.parentElement.style.display = ""; // afișează articolul
+        } else {
+            produs.parentElement.style.display = "none"; // ascunde articolul
+        }
+    });
+});
